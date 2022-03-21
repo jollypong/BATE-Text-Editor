8ec7959b0a2d58555b322e62300e6b959fc5dbed
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-21 01:01:57 -0600

    backup at end of day

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,6 @@
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
+import { registerSW } from './register-sw';
 import './database';
 import '../css/style.css';
 
@@ -31,3 +32,6 @@ if ('serviceWorker' in navigator) {
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
+// Call registerSW method
+registerSW();
